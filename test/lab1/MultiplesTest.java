cc5c554c87140d77553bb991b98c4d423cfdeea8
Aaliyah Pirani <Aaliyah@cadenceitsolutions.ca>
2025-09-08 21:09:43 -0400

    testing done and new method

--- test/lab1/MultiplesTest.java
+++ test/lab1/MultiplesTest.java
@@ -10,6 +10,7 @@ public class MultiplesTest {
     // TODO: When you are ready to test task X,
     //  copy this file into the test folder.
 
+
     @Test
     public void testFifteen() {
         assertEquals(7,
@@ -25,6 +26,7 @@ public class MultiplesTest {
                 "Multiples.multiples(10, 3, 5) is incorrect, the values" +
                         " it should be counting are: 3, 5, 6, 9");
     }
+
 
     @Test
     public void testThousand() {
